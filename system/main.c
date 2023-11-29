f047811191d47ea4ff144994a8f80c266f085626
Hari Prasad Kalluri <hkalluri@purdue.edu>
2023-11-28 23:51:26 -0500

    Benchmarks

--- system/main.c
+++ system/main.c
@@ -2,6 +2,8 @@
 
 #include <xinu.h>
 
+uint32 endtime = 0;
+
 void cpu_bound(){
 	while (clkcountermsec < 10000) {
         continue;
@@ -34,7 +36,41 @@ void mem_bound() {
 		}
 	}
 
-	return;
+	endtime = clkcountermsec;
+}
+
+void cpu_bound2(){
+	int x, i, j;
+	j = 0;
+	while (j < 50) {
+		for (i = 0; i < 1000000000; i++) {
+			x = i;
+		}
+		j++;
+	}
+	endtime = clkcountermsec;
+}
+
+void io_bound2(){
+	int j = 0;
+	while (j < 10000) {
+        int x, i;
+        for (i = 0; i < 10000; i++) {
+            x = i;
+        }
+        sleepms(5);
+		j++;
+    }
+	endtime = clkcountermsec;
+}
+
+void test_benchmark2(){
+	resume(create(cpu_bound2, 8192, 10, "cpu", 0));
+	resume(create(cpu_bound2, 8192, 10, "cpu", 0));
+	resume(create(io_bound2, 8192, 10, "io", 0));
+	resume(create(io_bound2, 8192, 10, "io", 0));
+	resume(create(mem_bound, 1000000, 10, "io", 0));
+	resume(create(mem_bound, 1000000, 10, "io", 0));
 }
 
 
@@ -64,6 +100,7 @@ process	main(void)
 	    }
 	}
 	*/
+	/*
 	kprintf("Process creation started\n");
 	resume(create(cpu_bound, 8192, 10, "cpu", 0));
 	resume(create(cpu_bound, 8192, 10, "cpu", 0));
@@ -75,5 +112,13 @@ process	main(void)
 	resume(create(mem_bound, 1000000, 10, "io", 0));
 	resume(create(mem_bound, 1000000, 10, "io", 0));
 	kprintf("Process creation done\n");
+	*/
+	uint32 begintime = clkcountermsec;
+
+	test_benchmark2();
+
+	sleep(2000);
+
+	kprintf("Total time taken: %d\n", endtime - begintime);
 	return OK;
 }
